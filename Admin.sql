e1635056e3308b15baae043a2af9cfa4683cf659
Hồ Phan Huy <131233459+huy2023software@users.noreply.github.com>
2023-05-25 13:48:52 +0700

    Add files via upload

---
+++ Admin.sql
@@ -0,0 +1,5 @@
+use CrimeFile
+go
+insert Administrator values 
+	('Admin001', 'Huy Ho Phan', 18, 'M', 'Add1', '0932961706', 'PassHuyHoPhan'),
+	('Admin002', 'Trong Luong Hoan', 18, 'M', 'Add2', '0123456789', 'PassTrongLuongHoan')
